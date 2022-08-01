a077fe818a261c0882a6fd5f6e28578d9d9a54f5
usbbush <jeanbienvenusb@gmail.com>
2022-08-01 23:03:42 +0200

    Add new package

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Calculator from './components/calculator';
 
 function App() {
   return (
-      <div></div>
+    <Calculator />
   );
 }
 
